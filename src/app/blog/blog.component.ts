7687e49a9a339e3f74b2660db36f81bf83ce70e4
DJCubed12 <djcubed12@gmail.com>
2023-08-30 18:51:51 -0500

    Copied over homepage, blog, and login pages

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'blog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.scss']
+})
+export class BlogComponent {
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [CommonModule, BannerComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
