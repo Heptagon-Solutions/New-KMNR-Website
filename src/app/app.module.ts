daa2530472766f42fe560d540d93b680c2883e0a
DJCubed12 <djcubed12@gmail.com>
2023-08-30 19:23:22 -0500

    Added TopBar & Footer. Moved Banner to shared folder. Fixed white border

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './shared/top-bar/top-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TopBarComponent,
+    FooterComponent
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BannerComponent } from './banner/banner.component';
+import { BannerComponent } from '../shared/banner/banner.component';
 
 @Component({
   selector: 'home',
