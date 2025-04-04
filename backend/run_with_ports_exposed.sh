556c0f7d00f9cbc89a1a190b29f16825d655c756
DJCubed12 <djcubed12@gmail.com>
2025-04-04 16:13:02 -0500

    Fix run_with_ports_exposed to autorestart

--- backend/run_with_ports_exposed.sh
+++ backend/run_with_ports_exposed.sh
@@ -2,4 +2,4 @@
 
 # Runs backend with ports exposed to LAN
 # Note that this is for development only; the flask command is NOT meant for production, and using `host=0.0.0.0` is not exactly secure.
-pipenv run python3 -m flask run --host=0.0.0.0
+pipenv run python3 -m flask run --debug --host=0.0.0.0
