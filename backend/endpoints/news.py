0684bf60d634ec41d631b78b5fbf658e32336117
DJCubed12 <djcubed12@gmail.com>
2025-11-23 23:55:24 -0600

    Pagination in town & campus news

--- backend/endpoints/news.py
+++ backend/endpoints/news.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from database import db, DatabaseError
 
@@ -6,12 +6,43 @@ from database import db, DatabaseError
 town_and_campus_news_blueprint = Blueprint("town_and_campus_news", __name__)
 
 
+DEFAULT_PAGE_SIZE = 200
+"""Default number of rows to fetch and return."""
+
+
+@town_and_campus_news_blueprint.get("/count/news")
+def dj_count():
+    try:
+        with db.connection.cursor() as cur:
+            cur.execute("""SELECT COUNT(`id`) AS `count` FROM `town_and_campus_news`""")
+            count = cur.fetchone()["count"]
+        return {"count": count}, 200
+    except DatabaseError as e:
+        return {"message": f"{e.args[1]} ({e.args[0]})"}, 500
+
+
 @town_and_campus_news_blueprint.route("/admin/news")
 def get_all_news():
     """Get all entries in town_and_campus_news in the database."""
+    count = request.args.get("count", default=DEFAULT_PAGE_SIZE, type=int)
+    page = request.args.get("page", default=0, type=int)
+    offset = page * count
+
+    if count < 1 or page < 0:
+        return {
+            "message": f"Count must be greater than 0 and Page must be 0 or greater."
+        }, 400
+
     try:
         with db.connection.cursor() as cur:
-            cur.execute("SELECT * FROM town_and_campus_news ORDER BY submit_date")
+            cur.execute(
+                """
+                SELECT * FROM town_and_campus_news
+                ORDER BY submit_date
+                LIMIT %s OFFSET %s
+                """,
+                (count, offset),
+            )
 
             entries_data = cur.fetchall()
 
@@ -34,6 +65,6 @@ def get_all_news():
                     "expirationDate": entry["expiration_date"],
                 }
             )
-        return entries
+        return {"townAndCampusNews": entries}
     except DatabaseError as e:
         return {"message": f"{e.args[1]} ({e.args[0]})"}, 500
