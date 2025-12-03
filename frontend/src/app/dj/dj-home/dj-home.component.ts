75860065ed5f13fc581ff316fcb76ac10440101a
DJCubed12 <djcubed12@gmail.com>
2025-12-03 00:40:20 -0600

    Move Spotify/Playlists stuff from admin home to DJ home

--- frontend/src/app/dj/dj-home/dj-home.component.ts
+++ frontend/src/app/dj/dj-home/dj-home.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { PlaylistProfile } from 'src/models/playlist';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { PlaylistService } from 'src/app/services/playlist.service';
 
 @Component({
   selector: 'dj-home',
@@ -9,4 +13,71 @@ import { RouterModule } from '@angular/router';
   templateUrl: './dj-home.component.html',
   styleUrls: ['./dj-home.component.scss'],
 })
-export class DJHomeComponent {}
+export class DJHomeComponent implements OnInit {
+  isSpotifyConnected = false;
+  playlists: PlaylistProfile[] = [];
+  isLoading = false;
+
+  constructor(
+    private readonly spotifyService: SpotifyService,
+    private readonly playlistService: PlaylistService
+  ) {}
+
+  ngOnInit() {
+    this.spotifyService.isAuthenticated$.subscribe(isAuth => {
+      this.isSpotifyConnected = isAuth;
+    });
+    this.loadPlaylists();
+  }
+
+  connectToSpotify() {
+    this.spotifyService.authorize();
+  }
+
+  loadPlaylists() {
+    this.isLoading = true;
+    this.playlistService.getPlaylists().subscribe({
+      next: playlists => {
+        this.playlists = playlists;
+        this.isLoading = false;
+      },
+      error: error => {
+        console.error('Failed to load playlists:', error);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  publishToSpotify(playlist: PlaylistProfile) {
+    if (!this.isSpotifyConnected) {
+      alert('Please connect to Spotify first');
+      return;
+    }
+
+    this.playlistService.publishToSpotify(playlist.id).subscribe({
+      next: response => {
+        playlist.spotifyPlaylistId = response.spotify_playlist_id;
+        alert(
+          `Playlist published to Spotify! ${response.tracks_added} tracks added.`
+        );
+
+        if (response.spotify_url) {
+          window.open(response.spotify_url, '_blank');
+        }
+      },
+      error: error => {
+        console.error('Failed to publish playlist:', error);
+        alert('Failed to publish playlist to Spotify');
+      },
+    });
+  }
+
+  openSpotifyPlaylist(spotifyPlaylistId: string) {
+    if (spotifyPlaylistId) {
+      window.open(
+        `https://open.spotify.com/playlist/${spotifyPlaylistId}`,
+        '_blank'
+      );
+    }
+  }
+}
