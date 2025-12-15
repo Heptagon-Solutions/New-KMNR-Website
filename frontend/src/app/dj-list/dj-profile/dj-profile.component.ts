26de1332427e8323e05be3a07e96a2d8c25b6d92
DJCubed12 <djcubed12@gmail.com>
2025-12-15 14:09:54 -0600

    Edit DJ names

--- frontend/src/app/dj-list/dj-profile/dj-profile.component.ts
+++ frontend/src/app/dj-list/dj-profile/dj-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { DJProfile } from 'src/models/dj';
 
@@ -11,11 +12,17 @@ import { ProfileImageComponent } from 'src/app/shared/profile-image/profile-imag
 @Component({
   selector: 'dj-profile',
   standalone: true,
-  imports: [CommonModule, ProfileImageComponent],
+  imports: [CommonModule, ProfileImageComponent, ReactiveFormsModule],
   templateUrl: './dj-profile.component.html',
   styleUrls: ['./dj-profile.component.scss'],
 })
 export class DJProfileComponent {
+  protected readonly newDJNameForm = new FormControl<string>('', {
+    validators: [Validators.required, Validators.minLength(1)],
+    nonNullable: true,
+  });
+  protected readonly newProfileDescForm = new FormControl<string>('');
+
   protected dj: DJProfile | undefined = undefined;
 
   protected isEditing: boolean = false;
@@ -33,9 +40,36 @@ export class DJProfileComponent {
     }
   }
 
+  protected startEditing() {
+    if (this.dj) {
+      this.newDJNameForm.setValue(this.dj.djName);
+      this.newProfileDescForm.setValue(this.dj.profileDesc);
+    }
+    this.isEditing = true;
+  }
+
+  protected updateProfile() {
+    if (this.dj && this.newDJNameForm.valid && this.newProfileDescForm.valid) {
+      const newDJName = this.newDJNameForm.value;
+      const newProfileDesc = this.newProfileDescForm.value;
+
+      this.djService.updateDJInfo(this.dj.id, newDJName, null).subscribe({
+        next: () => {
+          this.dj!.djName = newDJName;
+          this.isEditing = false;
+        },
+        error: (err: HttpErrorResponse) => {
+          alert(
+            `Could not update DJ Name:\n${err.status} ${err.statusText}: ${err.error?.message}`
+          );
+        },
+      });
+    }
+  }
+
   protected updateProfileImage(newImage: File | null | undefined) {
     if (this.dj && newImage) {
-      this.djService.updateDjProfileImg(this.dj.id, newImage).subscribe({
+      this.djService.updateDJProfileImg(this.dj.id, newImage).subscribe({
         next: newImagePath => {
           this.dj!.profileImg = newImagePath;
         },
