9eddcbdaa3c27c25351a4fa4cabe495175093889
DJCubed12 <djcubed12@gmail.com>
2025-12-16 14:54:41 -0600

    Reorganize admin news files

--- frontend/src/app/admin/admin-news/admin-news-edit/admin-news-edit.component.ts
+++ frontend/src/app/admin/admin-news/admin-news-edit/admin-news-edit.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'admin-news-edit',
   templateUrl: './admin-news-edit.component.html',
-  styleUrls: ['./admin-news-edit.component.scss']
+  styleUrls: ['./admin-news-edit.component.scss'],
+  standalone: true,
 })
-export class AdminNewsEditComponent {
-
-}
+export class AdminNewsEditComponent {}
