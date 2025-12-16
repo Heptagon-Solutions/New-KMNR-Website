0be510be697f859fa1ba89a9b956254d45cbd77b
DJCubed12 <djcubed12@gmail.com>
2025-12-16 17:05:10 -0600

    Remove OnAirComponent usages

--- frontend/src/app/shows/shows.component.ts
+++ frontend/src/app/shows/shows.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Semester } from 'src/models/general';
-
-import { ShowService } from '../services/show.service';
-import { OnAirComponent } from '../shared/on-air/on-air.component';
-
+import { ShowService } from 'src/app/services/show.service';
 import { ShowScheduleComponent } from './show-schedule/show-schedule.component';
 
 @Component({
   selector: 'shows',
   standalone: true,
-  imports: [CommonModule, OnAirComponent, ShowScheduleComponent],
+  imports: [CommonModule, ShowScheduleComponent],
   templateUrl: './shows.component.html',
   styleUrls: ['./shows.component.scss'],
 })
